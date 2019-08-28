1d080cf25299a5a0cdb79a8ee911f0313a773371
Nick Nance <nance.nick@gmail.com>
2019-08-27 22:28:43 -0700

    refactor app folder structure

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,8 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/styles";
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import theme from "./theme";
+import App from "./app/App";
+import theme from "./app/theme";
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
